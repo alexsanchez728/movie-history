7bc58e62c705d1c91829be85c4a4afc9aa813f71
Alex Sanchez <alesanchez728@gmail.com>
2017-10-17 21:07:24 -0500

    movie posters print in defined size closes #6

--- javascripts/tmdb.js
+++ javascripts/tmdb.js
@@ -1,6 +1,7 @@
 "use strict";
 
 let tmdbKey;
+let imgConfig;
 const dom = require("./dom");
 
 const searchTMDB = (query) => {
@@ -10,6 +11,25 @@ const searchTMDB = (query) => {
 		}).fail((error) => {
 			reject(error);
 		});
+	});
+};
+
+const tmdbConfiguration = () => {
+	return new Promise((resolve, reject) => {
+		$.ajax(`https://api.themoviedb.org/3/configuration?api_key=${tmdbKey}`).done((data) => {
+			resolve(data.images);
+		}).fail((error) => {
+			reject(error);
+		});
+	});
+};
+
+const getConfig = () => {
+	tmdbConfiguration().then((results) => {
+		imgConfig = results;
+		console.log("img info", imgConfig);
+	}).catch((error) => {
+		console.log("error in getConfig", error);
 	});
 };
 
@@ -23,11 +43,12 @@ const searchMovies = (query) => {
 
 const setKeys = (apiKey) => {
 	tmdbKey = apiKey;
+	getConfig();
 };
 
 const showResults = (movieArray) => {
 	dom.clearDom();
-	dom.domString(movieArray);
+	dom.domString(movieArray, imgConfig);
 };
 
 module.exports = {setKeys, searchMovies};
