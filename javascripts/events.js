6abca78880694d95d2ba97561ed92e189a3b6e42
Alex Sanchez <alesanchez728@gmail.com>
2017-10-23 18:44:32 -0500

    navbar link action closes #18

--- javascripts/events.js
+++ javascripts/events.js
@@ -14,4 +14,26 @@ const pressEnter = () => {
 
 };
 
-module.exports = {pressEnter};
+
+// Add function: myLinks - click events that checks the id of event.target and:
+const myLinks = () => {
+	$(document).click((e) => {
+		if (e.target.id === "navSearch") {
+			$("#search").removeClass("hide");
+			$("#myMovies").addClass("hide");
+			$("#authScreen").addClass("hide");
+		} else if (e.target.id === "mine") {
+			$("#search").addClass("hide");
+			$("#myMovies").removeClass("hide");
+			$("#authScreen").addClass("hide");
+		} else if (e.target.id === "authenticate") {
+			$("#search").addClass("hide");
+			$("#myMovies").addClass("hide");
+			$("#authScreen").removeClass("hide");
+		}
+
+	});
+
+};
+
+module.exports = {pressEnter, myLinks};
